d92694a9474b63c319ad35d95b1700c1fe7f1599
Robbie Lane <lane.robbie@gmail.com>
2015-11-08 19:06:14 -0700

    updated seed.rb

--- db/seeds.rb
+++ db/seeds.rb
@@ -18,28 +18,18 @@ Image.create!([
   {name: "Haines", link: "http://robbielane.net/works/haines/photos/HainesTananiPoint.jpg"}
 ])
 Idea.create!([
-  {title: "Title", body: "Body", user_id: 1, category_id: nil},
   {title: "New Idea", body: "Shabby chic photo booth microdosing master cleanse chambray organic. You probably haven't heard of them VHS shabby chic, hashtag pickled tacos tattooed plaid bitters flannel irony. ", user_id: 2, category_id: 2},
   {title: "Another Idea", body: "Salvia mlkshk viral, cred keytar yr readymade pabst selvage polaroid VHS flannel health goth disrupt. VHS etsy small batch, cred yuccie polaroid sriracha distillery you probably haven't heard of them raw denim fixie. Leggings quinoa kogi, umami humblebrag banjo sartorial listicle church-key banh mi typewriter.", user_id: 2, category_id: 3},
   {title: "Super Idea", body: "Tilde craft beer mustache kogi helvetica tacos, direct trade sartorial yr vinyl vice. Asymmetrical forage hella chicharrones gentrify, distillery squid truffaut keffiyeh scenester PBR&B whatever hashtag slow-carb sustainable. Tattooed beard mlkshk flexitarian.", user_id: 2, category_id: 3},
   {title: "Brilliant Idea", body: "Retro cray occupy, direct trade farm-to-table cronut cornhole kombucha tilde trust fund photo booth. Cronut kogi beard mlkshk, jean shorts +1 selvage single-origin coffee thundercats normcore butcher mixtape brooklyn pickled literally. Venmo chia migas, brunch XOXO knausgaard truffaut etsy vegan letterpress master cleanse aesthetic. Shoreditch mustache mixtape godard, pug helvetica small batch cornhole locavore portland chillwave neutra beard organic.", user_id: 2, category_id: 1},
-  {title: "Blah Blah Blah", body: "Shabby chic photo booth microdosing master cleanse chambray organic. You probably haven't heard of them VHS shabby chic, hashtag pickled tacos tattooed plaid bitters flannel irony. ", user_id: 3, category_id: 3}
+
 ])
 IdeasImage.create!([
-  {image_id: 3, idea_id: 5},
-  {image_id: 4, idea_id: 5},
-  {image_id: 5, idea_id: 7},
-  {image_id: 6, idea_id: 7},
-  {image_id: 7, idea_id: 7},
-  {image_id: 8, idea_id: 7},
-  {image_id: 1, idea_id: 7},
-  {image_id: 9, idea_id: 6},
-  {image_id: 12, idea_id: 6},
-  {image_id: 13, idea_id: 6},
-  {image_id: 10, idea_id: 8},
-  {image_id: 12, idea_id: 9},
-  {image_id: 13, idea_id: 10},
-  {image_id: 14, idea_id: 11},
-  {image_id: 13, idea_id: 13},
-  {image_id: 13, idea_id: 14}
+  {image_id: 3, idea_id: 4},
+  {image_id: 4, idea_id: 4},
+  {image_id: 5, idea_id: 2},
+  {image_id: 6, idea_id: 1},
+  {image_id: 2, idea_id: 2},
+  {image_id: 1, idea_id: 4},
+  {image_id: 1, idea_id: 4}
 ])
